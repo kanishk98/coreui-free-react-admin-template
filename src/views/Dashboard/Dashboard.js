91003d352f524ed1e849eaee616b763fa0b1e0ba
kanishk98 <kanishk.kakar@gmail.com>
2018-11-20 03:06:28 +0530

    Add table

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -8,20 +8,18 @@ import {
   ButtonToolbar,
   Card,
   CardBody,
-  CardFooter,
   CardHeader,
-  CardTitle,
   Col,
-  Dropdown,
-  DropdownItem,
-  DropdownMenu,
-  DropdownToggle,
   Progress,
   Row,
   Table,
 } from 'reactstrap';
+import ReactTable from 'react-table';
 import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
-import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities'
+import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities';
+import Constants from '../../Constants';
+import {renderIf} from './renderIf';
+import JsonTable from 'ts-react-json-table';
 
 // import Widget03 from '../../views/Widgets/Widget03'
 const Widget03 = lazy(() => import('../../views/Widgets/Widget03'));
@@ -461,13 +459,74 @@ class Dashboard extends Component {
     this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
 
     this.state = {
-      mainChart: mainChart,
       dropdownOpen: false,
       radioSelected: 2,
+      itemData: null,
     };
   }
 
+  moveItem = (item) => {
+
+  }
+
+  _onClickCell = (event, columnName, rowData) => {
+    console.log(columnName);
+    console.log(rowData);
+    if (columnName == 'localage' && rowData.localage >= 90) {
+      // re-route to a new page with details of suggestion
+      let response = window.confirm('Are you sure you want to move this item?');
+      // true if OK, false if Cancel
+      if (response) {
+        this.moveItem(rowData);    
+      } else {
+        console.log('user denied access');
+      }
+    }
+  }
+
+  predict(item) {
+    // make appropriate API request here
+  }
+
   componentDidMount() {
+    let url = null;
+    if (this.props.old) {
+      url = 'http://18.188.218.137/get-old';
+    } else {
+      url = 'http://18.188.218.137/get-items?location=delhi';
+    }
+    console.log(url);
+    fetch(url)
+      .then(async (res) => {
+        console.log(res);
+        let d = await res.json();
+        for (let index in d) {
+          let data = d[index];
+          delete data.Binding;
+          delete data.Brand;
+          delete data.Creator;
+          delete data.ProductTypeName;
+          delete data.companiesMoving;
+          delete data.location;
+          delete data.govSchemeSize;
+          delete data.supplyDelhi;
+          delete data.supplyKolkata;
+          delete data.supplyChennai;
+          delete data.supplyMumbai;
+          let title = data.Title.substring(0, 30) + "...";
+          data.Title = title;
+        }
+        let data = d;
+        if (data.age >= 90) {
+          // item needs prediction
+          this.predict(data);
+        }
+        console.log(data);
+        this.setState({ itemData: data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   toggle() {
@@ -483,9 +542,13 @@ class Dashboard extends Component {
   }
 
   render() {
-
+    console.log(this.state);
+    if (!this.state.itemData) {
+      return null;
+    }
     return (
       <div className="animated fadeIn">
+        <JsonTable onClickCell={this._onClickCell} rows={this.state.itemData} excludeColumns={['_id', 'Binding', 'Creator', 'ProductTypeName', 'companiesMoving', 'govSchemeSize', 'location', 'supplyChennai', 'supplyDelhi', 'supplyMumbai', 'supplyKolkata']} />
         <Row>
           <Col>
             <Card>
