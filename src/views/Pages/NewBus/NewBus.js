db372aca4cc93edbde4ee4b1dcab78c83193eb4b
kanishk98 <kanishk.kakar@gmail.com>
2018-11-23 16:03:59 +0530

    Add onChange button to Forms

--- src/views/Pages/NewBus/NewBus.js
+++ src/views/Pages/NewBus/NewBus.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Forms from '../../Base/Forms/Forms';
+import DefaultHeader from '../../../containers/DefaultLayout/DefaultHeader';
+import { AppHeader } from '@coreui/react';
 
 class NewBus extends React.Component {
     render() {
         return (
+            <div classname="animated fade-in">
+            <AppHeader>
+                <DefaultHeader />
+            </AppHeader>
             <Forms />
+            </div>
         );
     }
 }
