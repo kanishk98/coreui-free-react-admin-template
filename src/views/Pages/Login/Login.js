78bc2f01a7abc27519d3d806c3c7817cfa4ade53
Kanishk Kakar <kanishk.kakar@gmail.com>
2018-11-23 08:14:52 +0530

    Add Google login

--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import { GoogleLogin } from 'react-google-login';
 
 class Login extends Component {
   render() {
@@ -32,7 +33,8 @@ class Login extends Component {
                       </InputGroup>
                       <Row>
                         <Col xs="6">
-                          <Button color="primary" className="px-4">Login</Button>
+                          {/*<Button color="primary" className="px-4">Login</Button>}*/}
+                          <GoogleLogin buttonText="login" />
                         </Col>
                         <Col xs="6" className="text-right">
                           <Button color="link" className="px-0">Forgot password?</Button>
