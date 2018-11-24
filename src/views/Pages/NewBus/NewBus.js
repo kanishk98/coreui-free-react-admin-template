0944b23f27fabea660fb20512633b71b1ce78214
Kanishk Kakar <kanishk.kakar@gmail.com>
2018-11-24 11:38:13 +0530

    Auth flow implemented across all pages

--- src/views/Pages/NewBus/NewBus.js
+++ src/views/Pages/NewBus/NewBus.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import Forms from '../../Base/Forms/Forms';
 import DefaultHeader from '../../../containers/DefaultLayout/DefaultHeader';
+import { Redirect } from 'react-router-dom';
 import { AppHeader } from '@coreui/react';
+import App from '../../../App';
 
 class NewBus extends React.Component {
+
+    constructor(props) {
+        super(props);
+        // optimistically assume that the user is logged in
+        this.state = {
+            loggedIn: true,
+        }
+    }
+
+    async componentWillMount() {
+        const isLoggedIn = await App.isLoggedIn();
+        if (isLoggedIn != 'yes') {
+            this.setState({loggedIn: false});
+        }
+    }
+
     render() {
+        if (!this.state.loggedIn) {
+            return (
+                <Redirect to="/login" />
+            );
+        }
         return (
             <div className="animated fade-in">
             <AppHeader>
